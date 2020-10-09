97b6c7ccaa293e0e511896f23d20da6cce16a523
mauriciovancine <mauricio.vancine@gmail.com>
2020-10-09 00:24:23 -0300

    2020-10-19

--- 01_aulas/convert_pdf.R
+++ 01_aulas/convert_pdf.R
@@ -7,10 +7,10 @@ library(xaringan)
 library(tidyverse)
 
 # directory
-setwd("/home/mude/data/github/minicurso-tidyverse/01_aulas")
+setwd("01_aulas")
 dir(pattern = ".Rmd")
 
 # convert rmarkdown
-purrr::map(dir(pattern = ".Rmd")[6], chrome_print, timeout = 2000)
+purrr::map(dir(pattern = ".Rmd")[2], chrome_print, timeout = 2000)
 
 # end ---------------------------------------------------------------------
